296d85cc788c94dd1c4548f51096d26bfa554645
AnaAlbarracinc <anaisabel.albarracin@um.es>
2022-06-20 19:46:23 +0200

    .

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(dirname + '/dist/worher'));
+app.use(express.static(__dirname + '/dist/worher'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(dirname+'/dist/worher/index.html'));
+res.sendFile(path.join(__dirname+'/dist/worher/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
